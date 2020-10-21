5e6c690e84cd2457db847c2655c90958ecf99d9f
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2020-10-21 14:20:15 +0200

    Initial checkin

---
+++ Program.cs
@@ -0,0 +1,172 @@
+﻿namespace JoseJwtTester
+{
+    using System;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Text.Json;
+    using System.Text.Json.Serialization;
+    using Jose; // <PackageReference Include="jose-jwt" Version="2.6.0" />
+
+    public class ApplicationCreationRequest
+    {
+        [JsonPropertyName("tenantId")]
+        public string TenantId { get; set; }
+
+        [JsonPropertyName("subscriptionId")]
+        public Guid SubscriptionId { get; set; }
+
+        [JsonPropertyName("timeStamp")]
+        public DateTimeOffset TimeStamp { get; set; }
+
+        public override string ToString() => $"{nameof(ApplicationCreationRequest)} {this.TenantId} {this.SubscriptionId} {this.TimeStamp}";
+    }
+
+    public enum SignatureValidity { Invalid = 0, Valid = 1 }
+
+    class Program
+    {
+        public static string SignRequest<T>(string signingKey, T request)
+        {
+            var secretKey = Convert.FromBase64String(signingKey);
+
+            var payloadString = JsonSerializer.Serialize(request);
+            return JWT.Encode(payloadString, secretKey, JwsAlgorithm.HS256);
+        }
+
+        public static (SignatureValidity, T) ValidateRequest<T>(string verificationKey, string token)
+        {
+            var secretKey = Convert.FromBase64String(verificationKey);
+
+            try
+            {
+                var json = JWT.Decode(token, secretKey);
+                Console.WriteLine(json);
+
+                var req = JsonSerializer.Deserialize<T>(json);
+                return (SignatureValidity.Valid, req);
+            }
+            catch (IntegrityException)
+            {
+                return (SignatureValidity.Invalid, default(T));
+            }
+        }
+        
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+        public static string ByteArrayToString(byte[] ba)
+        {
+            return BitConverter.ToString(ba).Replace("-", "");
+        }
+
+        public static string hmac_sha256(string base64Key, string plaintext)
+        {
+            var hmac = HMAC.Create("HMACSHA256");
+            hmac.Key = Convert.FromBase64String(base64Key);
+            var hashed = hmac.ComputeHash(Encoding.UTF8.GetBytes(plaintext));
+            return Convert.ToBase64String(hashed);
+        }
+
+        static void Main(string[] args)
+        {
+            var signingKey = "pDzCAKG9KSaCWY2kLaqf0UWJ89i/gy/6IGvndSWe4eo=";
+
+            // Console.WriteLine($"HMAC: {hmac_sha256("pDzCAKG9KSaCWY2kLaqf0UWJ89i/gy/6IGvndSWe4eo=", "Hello world!")}");
+
+            var request = new ApplicationCreationRequest
+            {
+                SubscriptionId = Guid.Parse("fb7fdc26-b0e5-45b6-8119-7bc48bc12e4e"),
+                TenantId = "chgeuerfte.onmicrosoft.com",
+                // TimeStamp = DateTimeOffset.UtcNow,
+                TimeStamp = new DateTimeOffset(new DateTime(2020, 10, 21, 11, 36, 01, DateTimeKind.Utc)) // "2020-10-21T11:36:01Z"
+            };
+
+            string token = SignRequest(signingKey, request);
+            token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRJZCI6ImNoZ2V1ZXJmdGUub25taWNyb3NvZnQuY29tIiwic3Vic2NyaXB0aW9uSWQiOiJmYjdmZGMyNi1iMGU1LTQ1YjYtODExOS03YmM0OGJjMTJlNGUiLCJ0aW1lU3RhbXAiOiIyMDIwLTEwLTIxVDEyOjE3OjU4WiJ9.RBvM31ztyN5fvuwngD-6RvDGCchiEapClajUkGj7ZR8";
+
+            Console.WriteLine($".NET token: {token}");
+
+            bool noTampering = true;
+            var validationKey = noTampering ? signingKey : "aDzCAKG9KSaCWY2kLaqf0UWJ89i/gy/6IGvndSWe4eo=";
+
+            switch (ValidateRequest<ApplicationCreationRequest>(validationKey, token))
+            {
+                case (SignatureValidity.Valid, var r): 
+                    Console.WriteLine(r);
+                    break;
+                case (SignatureValidity.Invalid, _):
+                    Console.Error.WriteLine("Invalid request");
+                    break;
+            }
+
+            // FunctionalOverload();
+        }
+
+        #region A bit too much lambda
+
+        public static Func<T, string> RequestSigner<T>(string signingKey)
+        {
+            var secretKey = Convert.FromBase64String(signingKey);
+
+            return (T request) =>
+            {
+                var payloadString = JsonSerializer.Serialize(request);
+                return JWT.Encode(payloadString, secretKey, JwsAlgorithm.HS256);
+            };
+        }
+
+        public static Func<string, (SignatureValidity, T)> RequestVerifier<T>(string verificationKey)
+        {
+            var secretKey = Convert.FromBase64String(verificationKey);
+
+            return (string token) =>
+            {
+                try
+                {
+                    var json = JWT.Decode(token, secretKey);
+                    var req = JsonSerializer.Deserialize<T>(json);
+                    return (SignatureValidity.Valid, req);
+                }
+                catch (IntegrityException)
+                {
+                    return (SignatureValidity.Invalid, default(T));
+                }
+            };
+        }
+
+        static void FunctionalOverload()
+        {
+            var signingKey = "pDzCAKG9KSaCWY2kLaqf0UWJ89i/gy/6IGvndSWe4eo=";
+
+            var request = new ApplicationCreationRequest
+            {
+                SubscriptionId = Guid.Parse("fb7fdc26-b0e5-45b6-8119-7bc48bc12e4e"),
+                TenantId = "chgeuerfte.onmicrosoft.com",
+                TimeStamp = DateTimeOffset.UtcNow
+            };
+
+            string token = RequestSigner<ApplicationCreationRequest>(signingKey)(request);
+            Console.WriteLine(token);
+            bool noTampering = true;
+            var validationKey = noTampering ? signingKey : "aDzCAKG9KSaCWY2kLaqf0UWJ89i/gy/6IGvndSWe4eo=";
+
+            switch (RequestVerifier<ApplicationCreationRequest>(validationKey)(token))
+            {
+                case (SignatureValidity.Valid, var r):
+                    Console.WriteLine(r);
+                    break;
+                case (SignatureValidity.Invalid, _):
+                    Console.Error.WriteLine("Invalid request");
+                    break;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ README.md
@@ -0,0 +1,51 @@
+﻿
+# Demo for JOSE JSON object signing
+
+```bash
+#!/bin/bash
+
+# https://gist.github.com/indrayam/dd47bf6eef849a57c07016c0036f5207
+
+function base64_for_token {
+    local base64Content="$1"
+    # echo "$( echo -n "${base64Content}" | sed -E s/\+/-/ | sed -E s/\//_/ | sed -E s/=+$// )"
+
+    echo "$( echo -n "${base64Content}" | sed s/\+/-/g | sed -E sQ/Q_Qg | sed -E s/=+$//)"
+}
+
+function hmac_sha256 {
+    local base64Key="$1"
+    local plaintext="$2"
+
+    local hexkey="$( echo -n "${base64Key}" | base64 -d | od -t x1 -An | tr -d '\n ' )"
+    local base64hmac="$( echo -n "${plaintext}" | openssl dgst -sha256 -mac hmac -macopt "hexkey:${hexkey}" -binary | base64 --wrap=0 )"
+
+    echo "$( base64_for_token "${base64hmac}" )"
+}
+
+function json_to_base64 {
+    local jsonText="$1"
+    local encoded="$( echo -n "${jsonText}" | base64 --wrap=0 )"
+    echo "$( base64_for_token "${encoded}" )"
+}
+
+function sign_json {
+    local base64Key="$1"
+    local jsonPayloadText="$2"
+
+    local header="$( json_to_base64 '{"alg":"HS256","typ":"JWT"}' )"
+    local payloadBase64="$( json_to_base64 "${jsonPayloadText}" )"
+    local sig="$( hmac_sha256 "${base64Key}" "${header}.${payloadBase64}" )"
+
+    echo "${header}.${payloadBase64}.${sig}"
+}
+
+base64Key="pDzCAKG9KSaCWY2kLaqf0UWJ89i/gy/6IGvndSWe4eo="
+
+tenantID="chgeuerfte.onmicrosoft.com"
+subscriptionID="fb7fdc26-b0e5-45b6-8119-7bc48bc12e4e"
+timestamp="$( date --utc +"%Y-%m-%dT%H:%M:%SZ" )"
+#timestamp="2020-10-21T11:36:01+00:00"
+
+echo "$( sign_json "${base64Key}" "{\"tenantId\":\"${tenantID}\",\"subscriptionId\":\"${subscriptionID}\",\"timeStamp\":\"${timestamp}\"}" )"
+```
